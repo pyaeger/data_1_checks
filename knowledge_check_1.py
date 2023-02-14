e4f19a25d83e3624649e1582f3cdb7f04fad42bf
pyaeger <patrickometry@gmail.com>
2023-02-13 21:01:45 -0500

    this is for knowledge check 1

---
+++ knowledge_check_1.py
@@ -0,0 +1,64 @@
+#Knowledge Check 1
+#Student: Patrick Yaeger
+#Class: DA1 - Thursday Nights
+
+print('')
+#Part 1 ==========
+print('==========> PART 1')
+
+print('Hello World!')
+print('Don\'t mind me. I\'m just passing through.')
+print('So, please do lose the attitude. Okay?')
+print('Thanks.')
+
+
+
+
+
+print('')
+#Part 2 ==========
+print('==========> PART 2')
+
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+
+print('There are currently', len(planets), 'planets in our solar system:')
+for planet in planets:
+    print(planet)
+
+print('My homeworld is ' + planets[2] + '.')
+
+print('However, be careful as one is reall neurotic. No, it\'s not that one silly. It\'s ' + planets[5] + '.')
+
+
+
+
+
+
+print('')
+#Part 3 ==========
+print('==========> PART 3')
+
+first_dog = {'name': 'Bella', 'age': 14, 'breed': 'Labrador Retriever'}
+second_dog = {'name': 'Bodhi', 'age': 10, 'breed': 'Saint Bernard'}
+print('My first dog was a',first_dog['breed'],'named',first_dog['name'] + '.')
+print('She lived to be',first_dog['age'],'years old.')
+print('My second dog\'s name was',second_dog['name'] + '.')
+print('Gone too soon.')
+
+
+
+
+
+
+
+print('')
+#Part 4 ==========
+print('==========> PART 4')
+
+the_seasons = ('spring', 'summer', 'autumn', 'winter')
+print('There have always been four seasons:')
+for season in the_seasons:
+    print(season)
+print('By the way, your favorite season cannot be',the_seasons[1],'as that one is mine.')
+
+
